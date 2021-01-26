2f1fb04e336d33480c0e0c53474d2cef0d9c1b2b
NicolasPopovicz <55032174+NicolasPopovicz@users.noreply.github.com>
2021-01-26 17:12:54 -0300

    First commit

---
+++ js/modules/funcionamento.js
@@ -0,0 +1,37 @@
+﻿export default function initFuncionaento() {
+	const funcionamento  = document.querySelector('[data-semana]');
+	const diasSemana     = funcionamento.dataset.semana.split(',').map(Number);
+	const horarioSemana  = funcionamento.dataset.horario.split(',').map(Number);
+
+	const dataAgora    = new Date();
+	const diaAgora     = dataAgora.getDay(); 
+	const horarioAgora = dataAgora.getHours(); 
+
+	const semanaAberto  = diasSemana.indexOf(diaAgora) !== -1;
+
+	const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1])
+
+	if(semanaAberto && horarioAberto) {
+		funcionamento.classList.add ('aberto');
+	}
+}
+
+
+
+/*
+const agora = new Date();
+const futuro = new Date('Dec 24 2020 23:59');
+
+//console.log(agora.getDate());
+
+//console.log(futuro);
+
+function transformDay(tempo) {
+    return tempo / (24 * 60 * 60 * 1000); // Para converter em horas, basta remover o 24
+}
+
+const diasAgora  = transformDay(agora.getTime());
+const diasFuturo = transformDay(futuro.getTime());
+
+console.log(Math.floor(diasFuturo - diasAgora));
+*/
